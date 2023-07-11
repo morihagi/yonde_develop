f100e3a7920b1a5b4bf27b76ec2557481c53f2e8
morihagi <mrsk1204@gmail.com>
2023-07-11 06:19:39 -0700

    ✨ add: #3 トップページ追加

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "popper", to: "popper.js", preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
-pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.4.0/js/fontawesome.all.js"
