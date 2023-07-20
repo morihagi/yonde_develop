e3c750f61f058dbbfbade8529b8b95a55a54c64c
morihagi <mrsk1204@gmail.com>
2023-07-20 09:50:18 -0700

    👍 change: #50 googleでのユーザー登録・ログイン

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -28,6 +28,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super(scope)
   # end
 
+  protect_from_forgery except: :google_oauth2
+
   def google_oauth2
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env['omniauth.auth'])
