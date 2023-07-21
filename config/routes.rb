75d6a09488786ff45c0b26b20624590c401061d7
morihagi <mrsk1204@gmail.com>
2023-07-21 00:33:20 -0700

    ♻️ refactor: 画面のレスポンシブル化

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   root 'static_pages#top'
+  get '/modals/terms', to: 'modals#terms'
+  get '/modals/privacy_policy', to: 'modals#privacy_policy'
+
   devise_for :users, 
               controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
                             registrations: 'users/registrations'
