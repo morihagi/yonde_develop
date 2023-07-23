ebe61e668fe55f6f239501a8993b9bdc8346c434
morihagi <mrsk1204@gmail.com>
2023-07-23 11:56:04 -0700

    ✨ add: ログイン済みユーザーのみのアクセスを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,59 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[ new create ]
+  before_action :set_post, only: %i[edit update destroy]
+
   def index
+    @q = Post.ransack(params[:q])
+    @posts = @q.result(distinct: true).includes([:user, :bookmarks]).order(created_at: :desc).page(params[:page])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, success: t('defaults.message.created', item: Post.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_created', item: Post.model_name.human)
+      render :new
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post, success: t('defaults.message.updated', item: Post.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_updated', item: Post.model_name.human)
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @post.destroy!
+    redirect_to posts_path, succes: t('defaults.message.deleted', item: Post.model_name.human)
+  end
+
+  # def bookmarks
+  #   @q = current_user.bookmark_posts.ransack(params[:q])
+  #   @bookmark_posts = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
+  # end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :post_image, :post_image_cache)
+  end
+
+  def set_post
+    @post = current_user.posts.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
               controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
                             registrations: 'users/registrations'
                             }
+
+  resources :posts
 end
