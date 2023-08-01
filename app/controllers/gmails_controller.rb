b598bcda2e732eeadaca3436c6d706a6a0145339
morihagi <mrsk1204@gmail.com>
2023-07-31 21:31:30 -0700

    👍 change: #84 Gmailのアクション実行後、Postのpost_statusを変更

--- app/controllers/gmails_controller.rb
+++ app/controllers/gmails_controller.rb
@@ -26,6 +26,7 @@ class GmailsController < ApplicationController
     draft.message = message
 
     gmail.create_user_draft('me', draft, options: {authorization: current_user.tokens.last.access_token})
+    @post.update(post_status: 'gmail_draft')
     redirect_to posts_path, notice: '投稿メールをGmailの下書きボックスに保存しました'
   end
 
@@ -49,6 +50,7 @@ class GmailsController < ApplicationController
     draft.message = message
 
     gmail.send_user_message('me', message, options: { authorization: Token.last.access_token })
+    @post.update(post_status: 'gmail_sent')
     redirect_to posts_path, notice: '投稿メールを番組に送りました'
   end
 
@@ -60,16 +62,14 @@ class GmailsController < ApplicationController
 
   def google_secret
     scope = 'https://www.googleapis.com/auth/gmail.compose'
-    current_user_token = current_user.tokens.last
 
-    Google::APIClient::ClientSecrets.new
-    (
-      { web :
-        { access_token: current_user_token.access_token,
-          refresh_token: current_user_token.refresh_token,
-          client_id: ENV['GOOGLE_CLIENT_ID'],
-          client_secret: ENV['GOOGLE_CLIENT_SECRET'],
-          scope: scope
+    Google::APIClient::ClientSecrets.new(
+      { "web" =>
+        { "access_token" => current_user.tokens.last.access_token,
+          "refresh_token" => current_user.tokens.last.refresh_token,
+          "client_id" => ENV['GOOGLE_CLIENT_ID'],
+          "client_secret" => ENV['GOOGLE_CLIENT_SECRET'],
+          "scope" => scope
         }
       }
     )
