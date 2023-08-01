f08c3e647011b52716029a5b61bb52fedb732c9c
morihagi <mrsk1204@gmail.com>
2023-08-01 16:08:14 -0700

    👍 change: #79 未ログインのユーザーの投稿メール作成機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     get 'draft', to: 'gmails#post_draft'
     get 'send', to: 'gmails#post_send'
   end
+  get 'show_for_guest_user', to: 'posts#show_for_unregistered_user', as: :show_for_guest_user
   resource :profile, only: %i[ show edit update ]
   get '/profile/edit_for_goods', to: 'profiles#edit_for_goods'
   resource :user, only: %i[ show  edit update ]
