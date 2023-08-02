6e44f25e51afaad2fea393a81eaed9690ef4f1a5
morihagi <mrsk1204@gmail.com>
2023-08-02 14:26:20 -0700

    ♻️ refactor: #92 postのバッジの条件分けの冗長なコードを改善

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def post_status_badge(post)
+    status_classes = {
+      'draft' => 'badge text-bg-secondary',
+      'gmail_draft' => 'badge text-bg-success',
+      'gmail_sent' => 'badge text-bg-warning',
+      'archived' => 'badge bg-white text-red border '
+    }
+
+    status_class = status_classes[post.post_status]
+    content_tag(:div, post.post_status_text, class: status_class) if status_class
+  end
 end
