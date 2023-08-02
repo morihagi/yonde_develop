b0147bb761a29fcac8e96d9a852b3cbd8bfb80cb
morihagi <mrsk1204@gmail.com>
2023-08-02 13:07:00 -0700

    👍 change

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'popper', to: 'popper.js', preload: true
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.2.2/dist/js/tom-select.complete.js"
