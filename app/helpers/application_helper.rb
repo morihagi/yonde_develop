2a7919e7084bbdddd07e900ca68c2039c7c4cbad
morihagi <mrsk1204@gmail.com>
2023-08-19 11:25:00 +0900

    ♻️ refactor: フォームをレスポンシブルに対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,10 +12,6 @@ module ApplicationHelper
     tag.i(class: ["bi", "bi-#{icon_name}"])
   end
 
-  def icon_with_text(icon_name, text)
-    tag.span(icon(icon_name), class: "me-2") + tag.span(text)
-  end
-
   def turbo_stream_flash
     turbo_stream.update "flashes", partial: "flash"
   end
