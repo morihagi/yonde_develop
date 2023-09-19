4224aca5adfa0edef2bac81e6ac05de0cb1fa22b
morihagi <mrsk1204@gmail.com>
2023-09-18 18:14:22 -0700

    🐛 fix: #140 posts失敗時のpost#editのJSの挙動を修正

--- app/javascript/checkbox.js
+++ app/javascript/checkbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('turbo:load', function() {
+document.addEventListener('turbo:render', function() {
   // チェックボックス要素を取得
   var checkBox = document.getElementById('check_default');
 
