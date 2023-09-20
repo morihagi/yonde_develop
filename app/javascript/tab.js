7980709e1b367530117e06e99ab8d99b55b74ee3
morihagi <mrsk1204@gmail.com>
2023-09-19 22:42:18 -0700

    🐛 fix: #121 JSがサイドバーには作用しないように修正

--- app/javascript/tab.js
+++ app/javascript/tab.js
@@ -1,41 +1,7 @@
-// import Cookies from 'js-cookie';
-
-// document.addEventListener('turbo:load', () => {
-//   const tabs = document.querySelectorAll('.nav-link');
-
-//   tabs.forEach((tab) => {
-//     tab.addEventListener('click', (event) => {
-//       event.preventDefault();
-//       const tabId = event.target.getAttribute('href');
-
-//       // Save the ID of the selected tab to a Cookie
-//       Cookies.set('selectedTab', tabId);
-
-//       // Toggle the display of tabs
-//       document.querySelectorAll('.tab-pane').forEach((pane) => {
-//         pane.classList.remove('active');
-//       });
-
-//       document.querySelector(tabId).classList.add('active');
-//     });
-//   });
-
-//   // Open the saved tab on page reload
-//   const selectedTab = Cookies.get('selectedTab');
-
-//   if (selectedTab) {
-//     tabs.forEach((tab) => {
-//       if (tab.getAttribute('href') === selectedTab) {
-//         tab.click(); // Click the selected tab to make it active
-//       }
-//     });
-//   }
-// });
-
 import Cookies from 'js-cookie';
 
 document.addEventListener('turbo:load', () => {
-  const tabs = document.querySelectorAll('.nav-link');
+  const tabs = document.querySelectorAll('#myTab .nav-link');
 
   tabs.forEach((tab) => {
     tab.addEventListener('click', (event) => {
