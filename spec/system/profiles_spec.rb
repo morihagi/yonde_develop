a3b7aab1f0cfc42374452120156e376042abd271
morihagi <mrsk1204@gmail.com>
2023-09-21 14:44:06 -0700

    ✨ add: #64 システムテストの追加

---
+++ spec/system/profiles_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe "Profiles", type: :system do
+  let(:user) { create(:user) }
+  let(:profile) { create(:profile, user: user) }
+
+  before do
+    sign_in user
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get profile_path(profile)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get edit_profile_path(profile)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit_for_goods" do
+    it "returns http success" do
+      get profile_edit_for_goods_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+        { zip_code: '1234567', prefecture: 'Tokyo', city: 'Chiyoda', other_address: '1-1-1', radio_name: 'Radio', phone: '090-1234-5678', legal_name: 'Legal', residence_prefecture: 'Tokyo', residence_city: 'Chiyoda' }
+      }
+
+      it "updates the requested profile and redirects to the profile" do
+        patch profile_path, params: { profile: new_attributes }
+        expect(response).to redirect_to(profile_path)
+      end
+    end
+
+    context "with invalid parameters" do
+      let(:new_attributes) {
+        { zip_code: 'a', prefecture: nil, city: nil, other_address: nil, radio_name: nil, phone: nil, legal_name: nil, residence_prefecture: nil, residence_city: nil }
+      }
+
+      it "does not update the requested profile and returns unprocessable entity status" do
+        patch profile_path, params: { profile: new_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+end
+
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  describe "sign up process" do
+    it "signs up a new user with valid information" do
+      visit '/users/sign_up'
+
+      fill_in 'Eメール', with: 'test@example.com'
+      fill_in 'パスワード', with: 'password12'
+      fill_in 'パスワード（確認用）', with: 'password12'
+      find('#user_agreement').click
+
+      click_button 'ユーザー登録'
+
+      expect(page).to have_content('アカウント認証用のメールを送信しました。メール内のリンクからアカウントを有効化させてください。')
+    end
+
+    it "does not sign up a new user with invalid information" do
+      visit '/users/sign_up'
+
+      fill_in 'Eメール', with: ''
+      fill_in 'パスワード', with: ''
+      fill_in 'パスワード（確認用）', with: ''
+      find('.agreement').click
+      click_button 'ユーザー登録'
+
+      expect(page).to have_content("Eメールを入力してください")
+      expect(page).to have_content("Eメールが不正な値です")
+      expect(page).to have_content("パスワードを入力してください")
+      expect(page).to have_content("パスワードが不正な値です")
+      expect(page).to have_content("パスワードは8文字以上で入力してください")
+    end
+  end
+
+  describe "User login information page" do
+    let(:user) { create(:user) }
+
+    it "displays the user's profile page" do
+      sign_in user
+      visit user_path
+      expect(page).to have_content(user.email)
+    end
+  end
+end
